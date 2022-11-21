627fc765a99720548dea5f2a8d77d0e5cbada964
Yuri Ramalho <yuri_ramalho@edu.unifor.br>
2022-11-21 13:58:15 -0300

    Classe Calculadora com apenas declarações

---
+++ Calculadora.java
@@ -0,0 +1,34 @@
+public class Calculadora {
+
+    public double Soma(double n1, double n2) {
+
+    }
+
+    public double Subtracao(double n1, double n2) {
+
+    }
+
+    public double Multiplicacao(double n1, double n2) {
+
+    }
+
+    public double Divisao(double n1, double n2) {
+
+    }
+
+    public double RaizQuadrada(double n1) {
+
+    }
+
+    public double Exponencial(double n1, double n2) {
+
+    }
+
+    public double Teto(double n1) {
+
+    }
+
+    public double Piso(double n1) {
+
+    }
+}
